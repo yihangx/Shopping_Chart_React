b739d8d453f51fc98083341752935947436e3650
yihangx <yx141@duke.edu>
2020-01-14 01:06:45 -0500

    version 1.0

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counters from './components/counters'
+import "bootstrap/dist/css/bootstrap.css";
+
+const element = <h1>EL PSY CONGROO</h1>;
+ReactDOM.render(<Counters />, document.getElementById('root'));
